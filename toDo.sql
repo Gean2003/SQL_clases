dd62851616e32275e9fe581b0355cc35794527d9
gean <202114701@cibertec.edu.pe>
2022-09-21 22:25:49 -0500

    finish

--- toDo.sql
+++ toDo.sql
@@ -7,6 +7,7 @@ CREATE TABLE "toDo" (
 
 insert into "toDo"  (id, title_task, complete, description) values (3,'Mejorar logica de programacion', true, 'hacer Lose ejerciciso de leetcode')
 insert into "toDo"  (id, title_task, complete, description) values (4,'Cenar', true, 'Tienes qie alimentarte')
+insert into "toDo"  (id, title_task, complete, description) values (5,'Descansar', false, 'Necesitas descansar por tu salud mental')
 
 select * from "toDo";
 
